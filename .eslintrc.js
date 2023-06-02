ff58bbf6003d1e5ada8307f6d3a4ceb89fe748aa
pengpeng.zhao <pengpeng.zhao@ly.com>
2023-06-02 17:34:56 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended'
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+            jsx: true
+        },
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'vue'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        // vite打包时自动去除console和debugger,所以这里直接关掉检查
+        'no-console': 'off',
+        'no-debugger': 'off'
+    }
+};
